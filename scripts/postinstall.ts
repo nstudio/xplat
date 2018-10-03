40ee82e13fcd1fe30378c780cbd981c800b5a8da
Morten Anton Bach Sjøgren <msn@nota.dk>
2018-10-03 13:31:37 +0200

    fix: formatter-patch stopped working with newer @nrwl/schematics

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -15,13 +15,17 @@ export function updateConfig() {
         // update default
         ngCli.cli.defaultCollection = "@nstudio/schematics";
         fs.writeFileSync(ngCliConfigPath, JSON.stringify(ngCli, null, 2));
-
-        // Prevent Nrwl formatter from walking into {N} platforms folder
-        fixFormatter();
       }
     } catch (err) {
       console.warn('An issue was detected during installation: angular.json does not exist.');
     }
+  }
+
+  try {
+    // Prevent Nrwl formatter from walking into {N} platforms folder
+    fixFormatter();
+  } catch (err) {
+    console.log('An issue were detected during patching the nx-formatter', err);
   }
 }
 
@@ -31,26 +35,18 @@ export function fixFormatter() {
   const formatPath = path.join(process.cwd(), '/../..', '@nrwl/schematics/src/command-line/format.js');
   let formatContent = fs.readFileSync(formatPath, 'UTF-8');
 
-  // Old patch line, globs should've been wrapped in quotes.
-  const oldPatchLine = `    patterns.push("xplat\/**\/*.ts", "!apps/**/platforms/{android,ios}/**/*.ts");`;
-  const patchLine = `    patterns.push('\"xplat\/**\/*.ts\"', '\"!apps/**/platforms/{android,ios}/**/*.ts\"');`;
-  if (formatContent.indexOf(oldPatchLine) !== -1) {
-    console.log('Old patch for nx format have been applied, replace with new patch');
-    fs.writeFileSync(formatPath, formatContent.replace(oldPatchLine, patchLine));
-    return;
-  }
-
+  const patchLine = `    // PATCHED by @nstudio/xplat\n    patterns.push('"xplat/**/*.ts"', '"!apps/**/platforms/{android,ios}/**/*"');`;
   if (formatContent.indexOf(patchLine) !== -1) {
     console.log(`Patch for nx format have already been applied`);
     return;
   }
 
-  const patchRegExp = /(}\n)(^\s+switch \(command\) {)/m;
+  const patchRegExp = /(^\s+var chunkList)/m;
   if (!patchRegExp.test(formatContent)) {
     throw new Error(`Apply couldn't patch for nx format`);
   }
 
-  const newFormatContent = formatContent.replace(patchRegExp, `$1\n${patchLine}\n$2`);
+  const newFormatContent = formatContent.replace(patchRegExp, `${patchLine}\n$1`);
   if (formatContent !== newFormatContent) {
     fs.writeFileSync(formatPath, newFormatContent);
     console.log('Patch for nx format have been applied');
