536ccdd62dc7cbbd3f59b23f3dbebfd9f5f20727
Morten Anton Bach Sjøgren <msn@nota.dk>
2018-10-10 11:58:41 +0200

    fix(Testing): format nativescript test-app code

--- src/xplat/_testing_files/nativescript/app/app/home/home-routing.module.ts
+++ src/xplat/_testing_files/nativescript/app/app/home/home-routing.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from "@angular/core";
-import { Routes } from "@angular/router";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
 
-import { HomeComponent } from "./home.component";
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [
-    { path: "", component: HomeComponent }
-];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forChild(routes)],
-    exports: [NativeScriptRouterModule]
+  imports: [NativeScriptRouterModule.forChild(routes)],
+  exports: [NativeScriptRouterModule]
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
--- src/xplat/_testing_files/nativescript/app/app/home/home.module.ts
+++ src/xplat/_testing_files/nativescript/app/app/home/home.module.ts
@@ -1,19 +1,12 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
-import { HomeRoutingModule } from "./home-routing.module";
-import { HomeComponent } from "./home.component";
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-    imports: [
-        NativeScriptCommonModule,
-        HomeRoutingModule
-    ],
-    declarations: [
-        HomeComponent
-    ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  imports: [NativeScriptCommonModule, HomeRoutingModule],
+  declarations: [HomeComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class HomeModule { }
+export class HomeModule {}
