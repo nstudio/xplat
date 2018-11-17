621928371b719c10dfbb879f50a32432c86cd2e6
Nathan Walker <walkerrunpdx@gmail.com>
2018-11-16 16:29:24 -0800

    feat(generators): {N} HRM livesync routing navigation restore
    closes https://github.com/nstudio/xplat/issues/51

---
+++ src/migrations/update-7-0-1/update-7-0-1.ts
@@ -0,0 +1,64 @@
+import {
+  chain,
+  Rule,
+  SchematicContext,
+  Tree
+} from "@angular-devkit/schematics";
+import { join } from 'path';
+import * as fs from 'fs';
+
+import { getJsonFromFile, updateJsonFile, createOrUpdate, updateJsonInTree } from '../../utils';
+
+function updateNativeScriptApps(tree: Tree, context: SchematicContext) {
+  const appsDir = tree.getDir("apps");
+  const appFolders = appsDir.subdirs;
+  const nxConfigPath = `nx.json`;
+  const nxJson = getJsonFromFile(tree, nxConfigPath);
+  const npmScope = nxJson.npmScope;
+  const cwd = process.cwd();
+  // console.log('webpackConfig:',webpackConfig);
+  const mainPath = join(cwd, 'node_modules/@nstudio/schematics/src/app.nativescript/_files/app/main.ts');
+  // console.log('webpackConfigPath:', webpackConfigPath);
+  let mainFile = fs.readFileSync(mainPath, 'UTF-8');
+  mainFile = mainFile.replace('<%= npmScope %>', npmScope);
+
+  const hmrNavigationPath = join(cwd, 'node_modules/@nstudio/schematics/src/xplat/_nativescript_files/utils/livesync-navigation.ts');
+  let hmrNavigation = fs.readFileSync(hmrNavigationPath, 'UTF-8');
+
+  const utilsIndexPath = join(cwd, 'node_modules/@nstudio/schematics/src/xplat/_nativescript_files/utils/index.ts');
+  let utilsIndex = fs.readFileSync(utilsIndexPath, 'UTF-8');
+
+  // update {N} apps and configs
+  for (const dir of appFolders) {
+    // console.log(dir);
+    if (dir.indexOf("nativescript-") === 0) {
+      const appDir = `${appsDir.path}/${dir}`;
+      // console.log('appDir:', appDir); 
+
+      createOrUpdate(
+        tree,
+        `${appDir}/app/main.ts`,
+        mainFile
+      );
+    }
+  }
+
+  if (tree.exists('xplat/nativescript')) {
+    createOrUpdate(
+      tree,
+      `xplat/nativescript/utils/livesync-navigation.ts`,
+      hmrNavigation
+    );
+
+    createOrUpdate(
+      tree,
+      `xplat/nativescript/utils/index.ts`,
+      utilsIndex
+    );
+  }
+  return tree;
+}
+
+export default function(): Rule {
+  return chain([updateNativeScriptApps]);
+}
---
+++ src/xplat/_nativescript_files/utils/livesync-navigation.ts
@@ -0,0 +1,30 @@
+import { NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { onBeforeLivesync, onAfterLivesync } from 'nativescript-angular/platform-common';
+import { RouterExtensions } from 'nativescript-angular/router';
+
+let cachedUrl: string;
+export function attachLivesyncNavigation() {
+  onBeforeLivesync.subscribe(moduleRef => {
+      console.log('#### onBeforeLivesync');
+      if (moduleRef) {
+          const router = <Router>moduleRef.injector.get(Router);
+          cachedUrl = router.url;
+          console.log('-------> Caching URL: ' + cachedUrl);
+      }
+  });
+
+  onAfterLivesync.subscribe(({ moduleRef, error }) => {
+      console.log(`#### onAfterLivesync moduleRef: ${moduleRef} error: ${error}`);
+      if (moduleRef) {
+          const router = <RouterExtensions>moduleRef.injector.get(RouterExtensions);
+          const ngZone = <NgZone>moduleRef.injector.get(NgZone);
+          if (router && cachedUrl) {
+            ngZone.run(() => {
+              router.navigateByUrl(cachedUrl, { animated: false });
+            });
+          }
+      }
+  });
+}
+
