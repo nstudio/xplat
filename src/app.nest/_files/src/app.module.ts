72f0ddf912d1012c424b77437a5f5e68a512bc16
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2019-01-07 00:42:34 +0100

    feat(nest): code style updates, remove redundant libs (#70)

--- src/app.nest/_files/src/app.module.ts
+++ src/app.nest/_files/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class ApplicationModule {}
--- src/app.nest/_files/src/app.service.ts
+++ src/app.nest/_files/src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  get() {
-    return "Hello world!";
+  getHello(): string {
+    return 'Hello world!';
   }
 }
