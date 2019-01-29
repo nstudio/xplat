a753e08ded45c381240c2c3ab422b731a8925a23
Nathan Walker <walkerrunpdx@gmail.com>
2019-01-29 09:20:40 -0800

    fix(ionic): route module naming

--- src/app.ionic/_files/src/app/app-routing.module.ts
+++ src/app.ionic/_files/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' }
+  { path: 'home', loadChildren: './pages/home/home.module#HomeModule' }
 ];
 
 @NgModule({
