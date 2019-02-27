09a0736dc69a86e79b84bc648e47778ee0c13af0
Nathan Walker <walkerrunpdx@gmail.com>
2019-02-27 08:21:02 -0800

    feat(nativescript): 5.2 updates

--- src/app.nativescript/_files/app/main.ts
+++ src/app.nativescript/_files/app/main.ts
@@ -1,7 +1,6 @@
 // https://github.com/NativeScript/nativescript-dev-webpack/issues/660#issuecomment-422711983
 import 'reflect-metadata';
 import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
-import { AppOptions } from 'nativescript-angular/platform-common';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
 
@@ -11,25 +10,4 @@ if (typeof __UGLIFIED__ !== 'undefined' && __UGLIFIED__) {
   require('@angular/core').enableProdMode();
 }
 
-let options: AppOptions = {};
-if (module['hot']) {
-  <% if (routing || sample) { %>
-  // attach to livesync hooks and perform navigation
-  require('@<%= npmScope %>/nativescript').attachLivesyncNavigation();
-  <% } %>
-  const hmrUpdate = require('nativescript-dev-webpack/hmr').hmrUpdate;
-
-  options.hmrOptions = {
-    moduleTypeFactory: () => AppModule,
-    livesyncCallback: platformReboot => {
-      console.log('HMR: Sync...');
-      hmrUpdate();
-      setTimeout(platformReboot, 0);
-    }
-  };
-  hmrUpdate();
-
-  module['hot'].accept(['./app.module']);
-}
-
-platformNativeScriptDynamic(options).bootstrapModule(AppModule);
+platformNativeScriptDynamic().bootstrapModule(AppModule);
