6b0ca8089e4f04779df70e91d1d9ceaa44e4f691
Nathan Walker <walkerrunpdx@gmail.com>
2019-11-24 12:49:08 -0800

    feat(nativescript): 6.2 migration

--- packages/nativescript-angular/src/utils/versions.ts
+++ packages/nativescript-angular/src/utils/versions.ts
@@ -3,3 +3,4 @@ export const nsCoreVersion = '~6.2.0';
 export const nsNgVersion = '~8.20.0';
 export const nsNgFonticonVersion = '^4.2.0';
 export const nodeSassVersion = '^4.12.0';
+export const terserWebpackVersion = '~2.2.0';
