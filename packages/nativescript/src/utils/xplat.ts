b749f14872998fc53301a3ab1aa17762f81c35e0
Nathan Walker <walkerrunpdx@gmail.com>
2020-03-03 12:09:43 -0800

    chore(core): cleanup prettier

--- packages/nativescript/src/utils/xplat.ts
+++ packages/nativescript/src/utils/xplat.ts
@@ -35,8 +35,9 @@ export namespace XplatNativeScriptHelpers {
 **/apps/nativescript*/src/assets/*.min.css
 **/apps/*nativescript/hooks/**/*
 **/apps/*nativescript/tools/**/*
-**/apps/*nativescript/src/assets/*.min.css
-**/xplat/nativescript*/plugins/**/*`);
+**/apps/*nativescript/src/assets/*.css
+**/xplat/nativescript/scss/fonticons/*.css
+**/xplat/nativescript*/plugins/**/*`, '**/xplat/nativescript/scss/fonticons/*.css');
   }
 
   export function updateGitIgnore() {
