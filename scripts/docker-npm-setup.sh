6a54dee67cea67fa56378e9a146b8423a4296050
Dmitry Rodetsky <colirs.developer@gmail.com>
2020-03-24 23:14:20 -0600

    chore(core): cleaned up comments in scripts/docker-npm-setup.sh

--- scripts/docker-npm-setup.sh
+++ scripts/docker-npm-setup.sh
@@ -12,16 +12,3 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 apt-get update && apt-get install -y yarn
 
 yarn install --network-timeout 1000000 # Timeout needed for Windows (really slow)
-
-
-
-# curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-
-# # install yarn
-# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-# sudo apt-get update && sudo apt-get install -y yarn
-
-# yarn install --network-timeout 1000000 # Timeout needed for Windows (really slow)
