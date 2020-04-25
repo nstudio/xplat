384713b9414829ca9f5088eb4d289864c7a9472c
Nathan Walker <walkerrunpdx@gmail.com>
2020-04-24 23:12:14 -0700

    fix(nativescript): app generator option without structure (#194)
    closes https://github.com/nstudio/xplat/issues/192

--- packages/nativescript-angular/src/utils/versions.ts
+++ packages/nativescript-angular/src/utils/versions.ts
@@ -11,7 +11,7 @@ export const nodeSassVersion = '^4.12.0';
 export const terserWebpackVersion = '~2.2.0';
 export const tslibVersion = '~1.11.0';
 export const nsThemeCoreVersion = '^1.0.4';
-export const typescriptVersion = '~3.7.4';
+export const typescriptVersion = '~3.8.3';
 
 // in case workspace had not added angular yet
 // TODO: move deps to nstudio/xplat to ref from everywhere for all supported framework versions
