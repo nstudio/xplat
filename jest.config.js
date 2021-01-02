95123477a4379ac499f720a23b004647aa22278e
Nathan Walker <walkerrunpdx@gmail.com>
2021-01-02 10:19:55 -0800

    chore(core): ci

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
+  modulePathIgnorePatterns: [
+    "tmp",
+    "<rootDir>/test",
+    "<rootDir>/packages",
+    "collection/.*/files"
+  ],
+  testPathIgnorePatterns: [
+    "node_modules",
+    "<rootDir>/build/packages/angular/spec",
+    "webpack-configs"
+  ],
+  coverageReporters: [
+    "html"
+  ],
+  coverageDirectory: "coverage",
   testTimeout: 30000
 }
