e1ab3d4615763a73db257c890f0d043d5e7adcd9
Nathan Walker <walkerrunpdx@gmail.com>
2021-04-17 11:39:47 -0700

    feat(nativescript): webpack config

--- packages/nativescript-angular/src/schematics/application/_files/webpack.config.js
+++ packages/nativescript-angular/src/schematics/application/_files/webpack.config.js
@@ -3,5 +3,15 @@ const webpack = require("@nativescript/webpack");
 module.exports = (env) => {
 	webpack.init(env);
 
+  // ignore warnings from env base
+  webpack.chainWebpack(config => {
+    config.set(
+      'ignoreWarnings',
+      (config.get('ignoreWarnings') || []).concat([
+        /environments\/base/
+      ])
+    )
+  });
+  
 	return webpack.resolveConfig();
 };
