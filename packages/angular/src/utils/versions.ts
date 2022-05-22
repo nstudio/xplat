d3797ce108fb90c3d22b309ed70f6381364968e8
Nathan Walker <walkerrunpdx@gmail.com>
2022-05-22 16:36:23 -0700

    feat: Nx 14.1, {N} 8.2 + latest angular 13.3 (#309)

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -1,11 +1,11 @@
 export const xplatVersion = '*';
-export const nxVersion = '^13.0.0';
+export const nxVersion = '^14.0.0';
 export const angularVersion = '^13.0.0';
 export const angularDevkitVersion = '^13.0.0';
 export const ngxTranslateVersion = '~14.0.0';
 export const ngxTranslateHttpVersion = '~7.0.0';
 export const coreJsVersion = '^3.6.5';
-export const rxjsVersion = '^7.4.0';
+export const rxjsVersion = '^7.5.0';
 export const zonejsVersion = '~0.11.4';
 export const jestPresetAngular = '11.0.0';
-export const typesJest = '27.0.2';
+export const typesJest = '27.4.1';
--- packages/ionic-angular/src/utils/versions.ts
+++ packages/ionic-angular/src/utils/versions.ts
@@ -1,14 +1,14 @@
 export const xplatVersion = '*';
-export const ionicAngularVersion = '~6.0.0';
+export const ionicAngularVersion = '^6.0.0';
 export const capacitorVersion = '^3.0.0';
 export const capacitorPluginsVersion = '~1.0.2';
-export const ngDevKitArchitect = '^0.1301.2';
+export const ngDevKitArchitect = '^0.1303.0';
 export const ngDevKitBuild = '^13.0.0';
 export const ngDevKitCore = '^13.0.0';
 export const ngDevKitSchematics = '^13.0.0';
 export const tsLibVersion = '^2.0.0';
 
-export const ionicAngularToolkitVersion = '^5.0.0';
+export const ionicAngularToolkitVersion = '^6.0.0';
 export const jasmineCoreVersion = '~3.8.0';
 export const jasmineSpecVersion = '~6.0.0';
 export const karmaVersion = '~6.3.2';
@@ -17,12 +17,12 @@ export const karmaCoverageVersion = '~2.0.3';
 export const karmaCoverageInstanbulVersion = '~3.0.2';
 export const karmaJasmineVersion = '~4.0.0';
 export const karmaJasmineHtmlVersion = '^1.5.0';
-export const typescriptVersion = '~4.5.4';
+export const typescriptVersion = '~4.6.2';
 
 export const angularVersion = '^13.0.0';
 export const angularDevkitVersion = '^13.0.0';
 export const ngxTranslateVersion = '~14.0.0';
 export const ngxTranslateHttpVersion = '~7.0.0';
 export const coreJsVersion = '^3.6.5';
-export const rxjsVersion = '^7.4.0';
+export const rxjsVersion = '^7.5.0';
 export const zonejsVersion = '~0.11.4';
