e5539c1a77b9c87817e8f85daae991f597471fe6
Nathan Walker <walkerrunpdx@gmail.com>
2023-07-05 12:23:46 -0700

    chore: Nx 16.5

--- packages/nativescript-angular/src/utils/versions.ts
+++ packages/nativescript-angular/src/utils/versions.ts
@@ -5,14 +5,14 @@ export const nsNgFonticonVersion = '~7.0.0';
 export const nsWebpackVersion = '~5.0.0';
 export const sassVersion = '^1.30.0';
 export const nsThemeCoreVersion = '^1.0.4';
-export const typescriptVersion = '~4.8.0';
+export const typescriptVersion = '~5.0.0';
 
 // in case workspace had not added angular yet
 // TODO: move deps to nstudio/xplat to ref from everywhere for all supported framework versions
 export const angularVersion = '^16.0.0';
 export const ngToolsWebpack = '^16.0.0';
 export const ngxTranslateVersion = '~14.0.0';
-export const rxjsVersion = '^7.5.0';
+export const rxjsVersion = '^7.8.0';
 export const zonejsVersion = '~0.13.0';
 export const angularEsLintVersion = '^16.0.0';
 export const nsEsLintVersion = '~0.0.4';
